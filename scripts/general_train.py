9e3d924109cc2b5025dc22032dea26bceb4ea14b
Khuyagbaatar Batsuren <khuyagbaatar@gmail.com>
2023-05-31 03:26:03 +0800

    Update general_train.py

--- scripts/general_train.py
+++ scripts/general_train.py
@@ -62,8 +62,8 @@ def get_key_params(args):
                 type='AdamW', lr=1e-4, weight_decay=0.1,
                 ),
             add_train_loader_kwargs=add_train_loader_kwargs,
-            desired_batch_size=128,
-            base_batch_size=128,
+            desired_batch_size=64,
+            base_batch_size=64,
             )
     return params
 
